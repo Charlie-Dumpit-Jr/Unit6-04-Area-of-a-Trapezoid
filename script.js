3272a113cc50aa62844a11a32fe388191c9bd8d3
CharlieDumpitJr <charlie.jr.dumpit@ocsbstudent.ca>
2021-03-22 15:19:16 +0000

    Unit6-04 Area of a Trapezoid (2)

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let base1 = 0
 let base2 = 0
 let height = 0
 let sum = 0
-let results = 0
+let answer = 0
 
 document.getElementById('results').addEventListener('click', solve)
 
@@ -14,7 +14,7 @@ function solve () {
   base1 = parseInt(base1)
   base2 = parseInt(base2)
   height = parseInt(height)
-  sum = (base1 + base2) /2
+  sum = (base1 + base2) / 2
   answer = sum * height
 
   document.getElementById('answer').innerHTML = answer
