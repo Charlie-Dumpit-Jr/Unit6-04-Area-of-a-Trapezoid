1609e9e01ae25e2882257bf366bd1080815f4521
CharlieDumpitJr <charlie.jr.dumpit@ocsbstudent.ca>
2021-03-22 15:09:24 +0000

    Unit6-04: Area of a Trapezoid(1)

---
+++ script.js
@@ -0,0 +1,21 @@
+
+let base1 = 0
+let base2 = 0
+let height = 0
+let sum = 0
+let results = 0
+
+document.getElementById('results').addEventListener('click', solve)
+
+function solve () {
+  base1 = document.getElementById('base1').value
+  base2 = document.getElementById('base2').value
+  height = document.getElementById('height').value
+  base1 = parseInt(base1)
+  base2 = parseInt(base2)
+  height = parseInt(height)
+  sum = (base1 + base2) /2
+  answer = sum * height
+
+  document.getElementById('answer').innerHTML = answer
+}
